4e2f29566c759e479452361c6c1a39b71d8beb62
Arvin <ardalumpines@outlook.com>
2023-09-25 10:38:45 +0300

    add countdown timer script

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ typewriter
   .pauseFor(1500)
   .start();
 // *******************************************************************************
+
+// Countdown Timer
+
+let countdownDate = new Date("Nov 18, 2023 08:00:00").getTime();
+
+let timer = setInterval(function () {
+  let now = new Date().getTime();
+
+  let distance = countdownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById(
+    "countdown"
+  ).textContent = `${days} Days ${hours} Hours ${minutes}m ${seconds}s`;
+
+  if (distance < 0) {
+    clearInterval(timer);
+    document.getElementById("countdown").textContent = "Celebrated";
+  }
+}, 1000);
